d1d696c77dce9b3a9e6fe88746bd416c549ea9e2
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-09-04 14:55:03 +0800

    chore: setting up catalogue search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
+
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+
+      {/* Catalogue displaying cars */}
+      <div 
+        id='discover'
+        className='mt-12 padding-x padding-y max-width'
+      >
+        <div className='home__text-container'>
+          <h1 className='text-4xl'>Car Catalogue</h1>
+          <p>Explore the cars that you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar/>
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel'/>
+            <CustomFilter title='year'/>
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }
